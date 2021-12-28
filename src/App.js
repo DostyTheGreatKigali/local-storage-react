5fe277961033c7f4c2bd7ee52922fa4bde07e3ca
DostyTheGreatKigali <abumucal89@gmail.com>
2021-12-28 22:58:20 +0000

    Local storage implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [noteEditing, setNoteEditing] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const newNote = {
+      id: Math.random().toString(36).substr(2, 9),
+      text: e.target.note.value,
+    };
+    setNotes([...notes, newNote]);
+    e.target.note.value = "";
+  };
+
+  const deleteNote = (idToDelete) => {
+    const filteredNotes = notes.filter((note) => note.id !== idToDelete);
+    setNotes(filteredNotes);
+  };
+
+  useEffect(() => {
+    const json = JSON.stringify(notes);
+    localStorage.setItem("notes", json);
+  }, [notes]);
+
+  useEffect(() => {
+    const json = localStorage.getItem("notes");
+    const savedNotes = JSON.parse(json);
+    if (savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, []);
+
+  const submitEdits = (event, idToEdit) => {
+    event.preventDefault();
+    const updatedNotes = notes.map((note) => {
+      if (note.id === idToEdit) {
+        return {
+          id: note.id,
+          text: event.target.note.value,
+        };
+      } else {
+        return note;
+      }
+    });
+    setNotes(updatedNotes);
+    setNoteEditing("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Local Storage Demo</h1>
+      <form onSubmit={addNote}>
+      <input type="text" name="note" />
+      <input type="submit" value="Submit" />
+      {/* <input type="submit" value="Submit" disabled={!notes.length} /> */}
+    </form>
+    {notes.map((note) => (
+  <div key={note.id}>
+    {note.id !== noteEditing ? (
+      <div>{note.text}</div>
+      ) : (
+      <form onSubmit={(e) => submitEdits(e, note.id)}>
+        <textarea name="note" defaultValue={note.text}></textarea>
+        <button type="submit"> Submit Edits</button>
+      </form>
+    )}
+    <button onClick={() => deleteNote(note.id)}>delete</button>
+    <button onClick={() => setNoteEditing(note.id)}>edit</button>
+  </div>
+))}
     </div>
   );
-}
+};
 
 export default App;
